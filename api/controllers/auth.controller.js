a061cd6646e70e9e2188cfecfc6ef21e47372705
Kirubel Tesfaw <kirubeltesfaw@gmail.com>
2024-10-30 11:15:57 +0300

    complete update user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRETE,{expiresIn: '5'});
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRETE);
       const { password: pass, ...rest } = user._doc;
       res
         .cookie('access_token', token, { httpOnly: true })
@@ -61,7 +61,7 @@ export const google = async (req, res, next) => {
         avatar: req.body.photo,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRETE, {expiresIn: '5'});
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRETE);
       const { password: pass, ...rest } = newUser._doc;
       res
         .cookie('access_token', token, { httpOnly: true })
--- client/src/redux/User/userSlice.js
+++ client/src/redux/User/userSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     currentUser: null,
     loading: false,
@@ -20,9 +20,28 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserStart,
+  updateUserFailure,
+  updateUserSuccess,
+} = userSlice.actions;
 
 export default userSlice.reducer;
