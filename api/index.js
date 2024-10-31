496a541de34114a00b7584a12be8bb566dc6d7eb
Kirubel Tesfaw <kirubeltesfaw@gmail.com>
2024-10-31 16:11:29 +0300

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from './routes/listing.route.js';
 dotenv.config();
 
 mongoose
@@ -14,7 +15,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
 
 const app = express();
 
@@ -28,13 +28,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((error, req, res, next) => {
-    const statusCode = error.statusCode || 500;
-    const message = error.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
